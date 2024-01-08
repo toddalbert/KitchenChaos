f21e633eadd9c62e3614aa204bbc5cd33a82b645
Todd H. Albert <todd@bocacode.com>
2024-01-07 22:59:23 -0500

    spawning kitchen objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
 
+  [SerializeField] private Transform tomatoPrefab;
+  [SerializeField] private Transform counterTopPoint;
+
   public void Interact() {
     Debug.Log("Interact");
+    Transform totmatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
+    totmatoTransform.localPosition = Vector3.zero;
   }
 
 
