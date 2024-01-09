179d764b4080d076448a4a3ba1419d713c9eb352
Todd H. Albert <todd@bocacode.com>
2024-01-08 22:48:32 -0500

    play animation on open container

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,14 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ContainerCounter : BaseCounter {
 
+  public event EventHandler OnPlayerGrabObject;
+
   [SerializeField] private KitchenObject kitchenObjectSO;
 
   public override void Interact(Player player) {
-    Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-    kitchenObjectTransform.GetComponent<KitchenObjectMB>().SetKitchenObjectParent(player);
+    if (!player.HasKitchenObject()) {
+      Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+      kitchenObjectTransform.GetComponent<KitchenObjectMB>().SetKitchenObjectParent(player);
+
+      OnPlayerGrabObject?.Invoke(this, EventArgs.Empty);
+    }
   }
 
 }
