823e6abf5b6e0b943c07bb3c55a5f0b9f6c9776a
Todd H. Albert <todd@bocacode.com>
2024-01-08 22:57:24 -0500

    player can pickup items from counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -12,9 +12,17 @@ public class ClearCounter : BaseCounter {
       if (player.HasKitchenObject()) {
         // transfer object to counter
         player.GetKitchenObject().SetKitchenObjectParent(this);
+      } else {
+        // player not carrying anything
       }
     } else {
       // spot is already taken
+      if (player.HasKitchenObject()) {
+        // player is carrying something
+      } else {
+        // player not carrying anything
+        GetKitchenObject().SetKitchenObjectParent(player);
+      }
     }
   }
 
