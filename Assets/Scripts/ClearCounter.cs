aa34a9dca82a222a1b588ba00187d7c868b6ed83
Todd H. Albert <todd@bocacode.com>
2024-01-08 22:52:32 -0500

    fix clear counter selected; allow drop item

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,7 +7,15 @@ public class ClearCounter : BaseCounter {
   [SerializeField] private KitchenObject kitchenObjectSO;
 
   public override void Interact(Player player) {
-
+    if (!HasKitchenObject()) {
+      // can take an object
+      if (player.HasKitchenObject()) {
+        // transfer object to counter
+        player.GetKitchenObject().SetKitchenObjectParent(this);
+      }
+    } else {
+      // spot is already taken
+    }
   }
 
 }
